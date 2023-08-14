e7819e34e7970d2b20883dbac01913dd4b805847
KeremCaan <keremcaan@hotmail.com>
2023-08-14 11:48:28 +0300

    first commit

--- NeonAcademySwitch/ViewController.swift
+++ NeonAcademySwitch/ViewController.swift
@@ -6,12 +6,37 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let switchButton: UISwitch = UISwitch()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+    }
+    
+    func configureUI() {
+        
+        view.backgroundColor = .green
+        
+        view.addSubview(switchButton)
+        switchButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        switchButton.onTintColor = .purple
+        switchButton.thumbTintColor = .black
+        switchButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(200)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(100)
+        }
+    }
+    @objc func buttonTapped() {
+        if switchButton.isOn {
+            view.backgroundColor = .red
+        } else {
+            view.backgroundColor = .green
+        }
     }
 
 
